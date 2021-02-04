2db2eae68e5772ad827e3e1677ee8b0c8a6c5aaf
Rafael Teixeira <rafaelgteixeira@ua.pt>
2021-02-04 16:35:11 +0000

    Correction of timeseries classification and adition of timeseries regression lstm

--- nilmtk_models/lstm.py
+++ nilmtk_models/lstm.py
@@ -2,6 +2,7 @@ import warnings
 warnings.filterwarnings('ignore',category=FutureWarning)
 warnings.filterwarnings('ignore',category=RuntimeWarning)
 
+from nilmtk.disaggregate import Disaggregator
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
@@ -30,20 +31,21 @@ def matthews_correlation(y_true, y_pred):
     return numerator / (denominator + K.epsilon())
 
 class LSTM_RNN(Disaggregator):
-    def __init__(self, timeframe, timestep, overlap, cv=0.2 ):
+    def __init__(self, timeframe, timestep, overlap, predicted_column, cv=0.2 ):
         self.MODEL_NAME = 'LSTM RNN'
         self.cv = cv
         self.timeframe = timeframe
         self.timestep = timestep
         self.overlap = overlap
         self.model = {}
+        self.column = predicted_column
 
     def partial_fit(self, train_main, train_appliances, **load_kwargs):
         n_features = len(train_main[0].columns.values)
         n_past_examples = int(self.timeframe*60/self.timestep)
         print("Preparing the Training Data")
 
-        X_train = self.generate_timeseries(train_main[0], True)
+        X_train = self.generate_main_timeseries(train_main[0], False)
 
         X_train = X_train.reshape(X_train.shape[0], n_past_examples, n_features)
         
@@ -53,7 +55,7 @@ class LSTM_RNN(Disaggregator):
 
         for app_name, power in train_appliances:
             print("Preparing the Test Data")
-            y_train = self.generate_timeseries(power[0], False)
+            y_train = self.generate_appliance_timeseries(power[0])
             
             y_cv = y_train[int(len(y_train)*(1-self.cv)):]
             y_train = y_train[0:int(len(y_train)*(1-self.cv))]
@@ -61,20 +63,22 @@ class LSTM_RNN(Disaggregator):
             print("Training ", app_name, " in ", self.MODEL_NAME, " model\n", end="\r")
             
             model = Sequential()
-            model.add(LSTM(256, input_shape=(n_past_examples, n_features)))
-            model.add(Dense(units=1, activation='sigmoid'))
-            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[matthews_correlation])
-            model.fit(X_train, y_train, epochs=1, batch_size=72, validation_data=(X_cv, y_cv), verbose=2, shuffle=False)
+            model.add(LSTM(X_train.shape[1], input_shape=(n_past_examples, n_features)))
+            model.add(Dense(1))
+            model.compile(optimizer='adam', loss='mean_squared_error', metrics=["root_mean_squared_error"])
+            model.fit(X_train, y_train, epochs=1, batch_size=500, validation_data=(X_cv, y_cv), verbose=2, shuffle=False)
 
             self.model[app_name] = model
             
     def disaggregate_chunk(self, test_mains):
         test_predictions_list = []
         n_past_examples = int(self.timeframe*60/self.timestep)
+        n_features = len(test_mains[0].columns.values)
 
-        X_test = self.generate_timeseries(test_mains[0], True)
+        X_test = self.generate_main_timeseries(test_mains[0], True)
+        print(X_test.shape)
         X_test = X_test.reshape(X_test.shape[0], n_past_examples, n_features)
-
+        print(test_mains[0].shape)
         appliance_powers_dict = {}
 
         for i, app_name in enumerate(self.model):
@@ -100,15 +104,22 @@ class LSTM_RNN(Disaggregator):
         #TODO
         return
 
-    def generate_timeseries(self, df, is_mains):
+    def generate_main_timeseries(self, df, is_test):
+        columns = list(df.columns.values)
+
         one_second = pd.Timedelta(1, unit="s")
         timestep = pd.Timedelta(self.timestep, unit="s")
-        overlap_index =  self.timeframe*30 - int(self.timeframe*30*self.overlap) -1
-        objective_step = pd.Timedelta(self.timeframe*60, unit="s") - pd.Timedelta(self.timeframe*60*self.overlap, unit="s")
-
         current_time = df.index[0].round("s", ambiguous=False)
         current_index = 0
-        objective_time = current_time + pd.Timedelta(self.timeframe*60, unit="s")
+
+        if is_test:
+            overlap_index = 1
+            objective_step = self.timestep
+            objective_time = objective_time + self.timestep
+        else:
+            overlap_index =  int(self.timeframe*60*len(columns)/self.timestep - int(self.timeframe*60/self.timestep*len(columns)*self.overlap) -1)
+            objective_step = pd.Timedelta(self.timeframe*60, unit="s") - pd.Timedelta(self.timeframe*60*self.overlap, unit="s")
+            objective_time = current_time + pd.Timedelta(self.timeframe*60, unit="s")
 
         past_feature_vector = []
         aprox = 0
@@ -116,12 +127,14 @@ class LSTM_RNN(Disaggregator):
         behind = 0
         data = []
 
-        columns = list(df.columns.values)
-
         while current_index < len(df):
+
             feature_vector = []
             if len(past_feature_vector) != 0:
                 feature_vector = past_feature_vector[ overlap_index : -1]
+            elif len(past_feature_vector) == 0 and is_test:
+                feature_vector = [0 for i in range(0, int(self.timeframe*60*len(columns)/self.timestep -2))]
+                print(len(feature_vector))
 
             while current_time != objective_time and current_index < len(df):
                 index_time = df.index[current_index].round("s", ambiguous=False)
@@ -145,20 +158,58 @@ class LSTM_RNN(Disaggregator):
                         feature_vector.append((df[c[0]][c[1]][df.index[current_index]] + feature_vector[-len(columns)])/2)
                     current_time += timestep
                     arred += 1
+
             if current_index < len(df):
                 objective_time += objective_step
                 past_feature_vector = feature_vector
-                if is_mains:
-                    data.append(feature_vector)
+                data.append(feature_vector)
+                
+        
+        print("")
+        print("Aprox Values: ", aprox)
+        print("Arred values: ", arred)
+        print("Behind values:", behind)
+        print("")
+        return np.array(data)
+    
+    def generate_appliance_timeseries(self, df):
+        columns = list(df.columns.values)
+
+        one_second = pd.Timedelta(1, unit="s")
+        timestep = pd.Timedelta(self.timestep, unit="s")
+        current_time = df.index[0].round("s", ambiguous=False)
+        current_index = 0
+
+        objective_step = pd.Timedelta(self.timeframe*60, unit="s") - pd.Timedelta(self.timeframe*60*self.overlap, unit="s")
+        objective_time = current_time + pd.Timedelta(self.timeframe*60, unit="s")
+
+        aprox = 0
+        arred = 0
+        behind = 0
+        data = []
+
+        while current_index < len(df):
+
+            while current_time != objective_time and current_index < len(df):
+                index_time = df.index[current_index].round("s", ambiguous=False)
+                if index_time == current_time or index_time - one_second == current_time or index_time + one_second == current_time:
+                    current_index += 1
+                    current_time += timestep
+                    aprox += 1
+                elif current_time > index_time:
+                    if  current_index < len(df) -1:
+                        next_index = df.index[current_index+1].round("s", ambiguous=False)
+                        if next_index == current_time or next_index - one_second == current_time:
+                            current_time += timestep
+                            behind += 1
+                    current_index += 2
                 else:
-                    added = False
-                    for i in feature_vector:
-                        if i > 0:
-                            data.append(1)
-                            added = True
-                            break
-                    if not added:
-                        data.append(0)
+                    current_time += timestep
+                    arred += 1
+
+            if current_index < len(df):
+                objective_time += objective_step
+                data.append(df[self.column[0]][self.column[1]][df.index[current_index]])
         
         print("")
         print("Aprox Values: ", aprox)
