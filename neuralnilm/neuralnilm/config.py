3d30ff446003dbe694c42e9112b867a5980fe0de
Rafael Teixeira <rafaelgteixeira@ua.pt>
2021-03-03 18:58:44 +0000

    Clean Up

--- neuralnilm/neuralnilm/config.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function, division
-import os
-import configparser
-
-config = configparser.RawConfigParser()
-filename = os.path.expanduser('~/.neuralnilm')
-config.read(filename)
